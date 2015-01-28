0c184a347d07363d406b3259b6a268571ef933b0
Barnaby Stewart <p14167376@myemail.dmu.ac.uk>
2015-01-28 08:03:12 +0000

    Added functionality for all but powerset (none tested)

--- set.c
+++ set.c
@@ -27,32 +27,38 @@ set * new_set(printer item_printer, equals item_compare)
 int set_isempty(set *s)
 {
 	assert(s!=NULL);
-	return 0; // needs to be implemented
+	return clist_isempty(s->items);
 }
 
 int set_isSubset(set *s, set * t)
 {
 	assert(s!=NULL);
-	clist_goto_head(s->items);
-	while (clist_cursor_inlist(s->items))
+	assert(t!=NULL);
+
+	if (set_isSubsetEq(s, t))
 	{
-		if (!set_isin(t,clist_get_item(s->items)))
-			return 0; // if not in t then not a subset
-		else
-			clist_goto_next(s->items);
+		return set_count(s) < set_count(t); // all in t, but check t is larger than s
 	}
-	return set_count(s) < set_count(t); // all in t, but check t is larger than s
+	return 0;
 }
 
 int set_isEqualTo(set *s, set * t)
 {
 	assert(s!=NULL);
-	return set_isSubsetEq(s,t) && set_isSubsetEq(t,s);
+	assert(t!=NULL);
+
+	if (set_isSubsetEq(s, t))
+	{
+		return set_count(s) == set_count(t); // all in t, but check size is same
+	}
+	return 0;
 }
 
 int set_isSubsetEq(set *s, set * t)
 {
 	assert(s!=NULL);
+	assert(t!=NULL);
+
 	clist_goto_head(s->items);
 	while (clist_cursor_inlist(s->items))
 	{
@@ -93,31 +99,69 @@ void set_insertInto(set *s, any x) // s = s u {x}
 
 void set_removeFrom(set *s, any x) // s = s \ {x}
 {
-// needs to be implemented
+	assert(s!=NULL);
+	if (set_isin(s,x)) // the cursor will be at the found item
+	{
+		clist_delete(s->items);
+	}
 }
 
 void set_intersectWith(set *s, set * t) // s = s n t t is unchanged
 {
 	assert(s!=NULL);
-	// needs to be implemented
+	assert(t!=NULL);
+	clist_goto_head(s->items);
+	while (clist_cursor_inlist(s->items))
+	{
+		if (!set_isin(t,clist_get_item(s->items)))
+			clist_delete (s->items);
+		else
+			clist_goto_next(s->items);
+	}
 }
 
 void set_unionWith(set *s, set * t) // s = s u t t is unchanged
 {
 	assert(s!=NULL);
-	// needs to be implemented
+	assert(t!=NULL);
+	clist_goto_head(t->items);
+	while (clist_cursor_inlist(t->items))
+	{
+		set_insertInto (s, clist_get_item(t->items));
+	}
 }
 
 void set_minusWith(set *s, set * t) // s = s \ t t is unchanged
 {
 	assert(s!=NULL);
-	// needs to be implemented
+	assert(t!=NULL);
+	clist_goto_head(s->items);
+	while (clist_cursor_inlist(s->items))
+	{
+		if (set_isin(t,clist_get_item(s->items)))
+			clist_delete (s->items);
+		else
+			clist_goto_next(s->items);
+	}
 }
 
 set* set_powerset(set *s) // generates new set
 {
 	assert(s!=NULL);
-	return NULL; // needs to be implemented
+
+	clist* powerset = new_clist();
+/*
+	clist_goto_head(s->items);
+	while (clist_cursor_inlist(s->items))
+	{
+		if (set_isin(t,clist_get_item(s->items)))
+			clist_delete (s->items);
+		else
+			clist_goto_next(s->items);
+	}
+*/
+
+	return powerset;
 }
 
 void set_print(set *s)
